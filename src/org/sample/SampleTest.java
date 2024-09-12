fd779515b8ac41ceb378918a8f2060b96d20c60a
Jayanthi086 <jayanthi086@gmail.com>
2024-09-12 15:44:52 +0530

    checking project

---
+++ src/org/sample/SampleTest.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class SampleTest {
+	public static void main(String[] args) {
+		System.out.println("login account details");
+		System.out.println("username:jayanthi");
+		System.out.println("password:48979879");
+		
+	}
+
+}
